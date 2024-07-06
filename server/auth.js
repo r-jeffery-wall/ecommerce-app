144dfae7d802af8348a28cd4dd325d9712faba6e
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-07-06 11:35:44 +0100

    Fixed password auth not working.

--- server/auth.js
+++ server/auth.js
@@ -4,13 +4,13 @@ const bcrypt = require('bcrypt');
 
 const setupAuth =  new LocalStrategy(
     (username, password, done) => {
-    db.findUserByUsername(username, (err, user) => {
+    db.findUserByUsername(username, async (err, user) => {
         // Error in retrieving user from DB.
         if (err) return done(err);
         // User not found.
         if (!user) return done(null, false, { message: "username not found." });
         //User found, but password is incorrect.
-        if (!bcrypt.compare(password, user.password)) return done(null, false, { message: "password incorrect."})
+        if (!await bcrypt.compare(password, user.password)) return done(null, false, { message: "password incorrect."})
         // User found and password valid.
         return done(null, user);
     });
