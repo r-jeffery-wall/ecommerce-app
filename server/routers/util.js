c1d5e188bb38da6ffd127e8ffa36c7f01da37a34
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-07-12 10:48:12 +0100

    Added admin check to authentication check.

--- server/routers/util.js
+++ server/routers/util.js
@@ -1,8 +1,9 @@
 // Middleware to check for logged in user.
 
 const checkAuth = async (req, res, next) => {
-    console.log(req.user.id, req.params.id)
-    if (!req.user) {
+    if (req.user.admin) { // Admins can interact with any user-restricted endpoint.
+        next()
+    } else if (!req.user) {
         res.status(400).send('You must be logged in to view this resource.')
     } else if (req.user.id != req.params.id) {
         res.status(403).send('You are not authorised to access this resource.')
