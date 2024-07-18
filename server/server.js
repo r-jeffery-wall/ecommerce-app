1a3bee991010d4c03f6e89179d367d504664625e
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-07-18 20:44:41 +0100

    Renamed user route to users.

--- server/server.js
+++ server/server.js
@@ -1,72 +1,82 @@
-const express = require('express');
-const parser = require('body-parser');
-const auth = require('./auth');
-const session = require('express-session');
-const usersDb = require('./db/users');
-const passport = require('passport');
-const morgan = require('morgan');
+const express = require("express");
+const parser = require("body-parser");
+const auth = require("./auth");
+const session = require("express-session");
+const usersDb = require("./db/users");
+const passport = require("passport");
+const morgan = require("morgan");
 
 //Routers
-const productsRouter = require('./routers/products');
-const categoriesRouter = require('./routers/categories');
-const usersRouter = require('./routers/user');
-const cartsRouter = require('./routers/carts');
-const ordersRouter = require('./routers/orders');
+const productsRouter = require("./routers/products");
+const categoriesRouter = require("./routers/categories");
+const usersRouter = require("./routers/users");
+const cartsRouter = require("./routers/carts");
+const ordersRouter = require("./routers/orders");
 const app = express();
 
 // Variables
 const PORT = 4001;
 
 // Middlewares
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(parser.json());
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
-    proxy: true
-}));
+    proxy: true,
+  }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    res.status(status).send(err)
-})
+  const status = err.status || 500;
+  res.status(status).send(err);
+});
 
 // Router set-up
-app.use('/products', productsRouter);
-app.use('/categories', categoriesRouter);
-app.use('/users', usersRouter);
-app.use('/carts', cartsRouter);
-app.use('/orders', ordersRouter);
+app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
+app.use("/users", usersRouter);
+app.use("/carts", cartsRouter);
+app.use("/orders", ordersRouter);
 
 // Set-up local strategy
 passport.use(auth.setupAuth);
 passport.serializeUser((user, done) => {
-    return done(null, user.id);
-})
+  return done(null, user.id);
+});
 passport.deserializeUser((id, done) => {
-    usersDb.findUserById(id, (err, user) => {
-        if (err) return done(err);
-        return done(null, user)
-    })
-})
+  usersDb.findUserById(id, (err, user) => {
+    if (err) return done(err);
+    return done(null, user);
+  });
+});
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: "Welcome to this simple generic e-commerce API."})
-})
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Welcome to this simple generic e-commerce API." });
+});
 
-app.post('/login', passport.authenticate('local', {failWithError: true}), (req, res) => {
-    res.status(200).send(`${req.user.username} logged in.`)
-})
+app.post(
+  "/login",
+  passport.authenticate("local", { failWithError: true }),
+  (req, res) => {
+    res.status(200).send(`${req.user.username} logged in.`);
+  },
+);
 
-app.get('/logout', (req, res, next) => {
-    req.logout((err) => {
-        if (err) { return next(err) }
-    })
-    res.status(200).send("User logged out.");
-})
+app.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+  });
+  res.status(200).send("User logged out.");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
+  console.log(`Server is listening on port ${PORT}`);
 });
