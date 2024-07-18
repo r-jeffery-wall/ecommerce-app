aa0dd7e6d84eb65428c5d6fde9238fed193eec1e
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-07-18 20:11:32 +0100

    Added endpoint to get all carts.

--- server/db/carts.js
+++ server/db/carts.js
@@ -1,82 +1,117 @@
-const pool = require('./dbConnect');
+const pool = require("./dbConnect");
+
+// Get all carts
+const getAllCarts = async (req, res) => {
+  await pool.query("SELECT * FROM carts", (err, results) => {
+    if (err) {
+      res.status(500).send();
+    }
+    res.status(200).send(results.rows);
+  });
+};
 
 // Get information on the current user's cart.
 const getLoggedInUserCart = async (req, res) => {
-    const user = req.params.userId;
-    await pool.query('SELECT * FROM carts WHERE user_id = $1', [user], (err, results) => {
-        if (err) {
-            res.status(500).send(err)
-        }
-        if (!results.rows[0]) {
-            res.status(404).send('User or cart does not exist.')
-        }
-        res.status(200).send(results.rows[0])
-    })
-}
+  const user = req.params.userId;
+  await pool.query(
+    "SELECT * FROM carts WHERE user_id = $1",
+    [user],
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      if (!results.rows[0]) {
+        res.status(404).send("User or cart does not exist.");
+      }
+      res.status(200).send(results.rows[0]);
+    },
+  );
+};
 
 // Add an item to a user's cart, if the cart does not exist it will be created.
 const addItemToCart = async (req, res) => {
-    const user = req.params.userId;
-    const { itemId } = req.body;
-    const currentCart = await getCurrentCart(user);
-    console.log(currentCart);
-    if (!currentCart) {
-        await pool.query('INSERT INTO carts(user_id, items) VALUES($1, $2) RETURNING *', [user, itemId], (err, results) => {
-            if (err) {
-                res.status(500).send(err)
-            }
-            res.status(200).send(results.rows[0])
-        })
-    } else {
-        const newCart = currentCart + ", " + itemId
-        await pool.query('UPDATE carts SET items = $1 WHERE user_id = $2 RETURNING *', [newCart, user], (err, results) => {
-            if (err) {
-                res.status(500).send(err)
-            }
-            res.status(200).send(results.rows[0])
-        })
-    }
-}
+  const user = req.params.userId;
+  const { itemId } = req.body;
+  const currentCart = await getCurrentCart(user);
+  console.log(currentCart);
+  if (!currentCart) {
+    await pool.query(
+      "INSERT INTO carts(user_id, items) VALUES($1, $2) RETURNING *",
+      [user, itemId],
+      (err, results) => {
+        if (err) {
+          res.status(500).send(err);
+        }
+        res.status(200).send(results.rows[0]);
+      },
+    );
+  } else {
+    const newCart = currentCart + ", " + itemId;
+    await pool.query(
+      "UPDATE carts SET items = $1 WHERE user_id = $2 RETURNING *",
+      [newCart, user],
+      (err, results) => {
+        if (err) {
+          res.status(500).send(err);
+        }
+        res.status(200).send(results.rows[0]);
+      },
+    );
+  }
+};
 
 // Delete a logged in user's cart.
 const deleteLoggedInUserCart = async (req, res) => {
-    const user = req.user.id;
-    await pool.query('DELETE FROM carts WHERE user_id = $1', [user], (err, results) => {
-        if (err) {
-            res.status(500).send(err)
-        }
-        res.status(204).send('Record deleted successfully.')
-    })
-}
+  const user = req.user.id;
+  await pool.query(
+    "DELETE FROM carts WHERE user_id = $1",
+    [user],
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      res.status(204).send("Record deleted successfully.");
+    },
+  );
+};
 
 // Checkout with the logged in user's cart.
 const checkoutCurrentCart = async (req, res, next) => {
-    const user = req.user.id;
-    const address = req.user.address;
-    const items = await getCurrentCart(user);
-    const { price } = req.body // I'm assuming this will be calculated client-side.
+  const user = req.user.id;
+  const address = req.user.address;
+  const items = await getCurrentCart(user);
+  const { price } = req.body; // I'm assuming this will be calculated client-side.
 
-    const results = await pool.query('INSERT INTO orders(user_id, items, delivery_address, price) VALUES($1, $2, $3, $4) RETURNING * ', [user, items, address, price])
+  const results = await pool.query(
+    "INSERT INTO orders(user_id, items, delivery_address, price) VALUES($1, $2, $3, $4) RETURNING * ",
+    [user, items, address, price],
+  );
 
-    await pool.query('DELETE FROM carts WHERE user_id = $1', [user], (err) => {
-        if (err) {
-            res.status(500).send(err)
-        }
-        res.status(200).send(`Order successfully placed: ${JSON.stringify(results.rows[0])}`)
-    })
-}
-
-const getCurrentCart = async (user,) => {
-    const results = await pool.query('SELECT items FROM carts WHERE user_id = $1', [user])
-    if (results.rows[0]) {
-        return results.rows[0].items
-    } else {
-        return null
+  await pool.query("DELETE FROM carts WHERE user_id = $1", [user], (err) => {
+    if (err) {
+      res.status(500).send(err);
     }
-}
+    res
+      .status(200)
+      .send(`Order successfully placed: ${JSON.stringify(results.rows[0])}`);
+  });
+};
+
+const getCurrentCart = async (user) => {
+  const results = await pool.query(
+    "SELECT items FROM carts WHERE user_id = $1",
+    [user],
+  );
+  if (results.rows[0]) {
+    return results.rows[0].items;
+  } else {
+    return null;
+  }
+};
 module.exports = {
-    getLoggedInUserCart,
-    addItemToCart,
-    deleteLoggedInUserCart,
-    checkoutCurrentCart
-}
+  getAllCarts,
+  getLoggedInUserCart,
+  addItemToCart,
+  deleteLoggedInUserCart,
+  checkoutCurrentCart,
+};
--- server/routers/carts.js
+++ server/routers/carts.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const db = require('../db/carts');
-const util = require('./util');
-
+const express = require("express");
+const db = require("../db/carts");
+const util = require("./util");
 
 const cartsRouter = express.Router();
 
+// Get all carts
+cartsRouter.get("/", util.checkAdmin, db.getAllCarts);
+
 // Get Logged in user cart.
-cartsRouter.get('/:userId', util.checkAuth, db.getLoggedInUserCart);
+cartsRouter.get("/:userId", util.checkAuth, db.getLoggedInUserCart);
 
 // Add item to user cart
-cartsRouter.post('/:userId', util.checkAuth, db.addItemToCart);
+cartsRouter.post("/:userId", util.checkAuth, db.addItemToCart);
 
 // Delete logged in user cart.
-cartsRouter.delete('/:userId', util.checkAuth, db.deleteLoggedInUserCart);
+cartsRouter.delete("/:userId", util.checkAuth, db.deleteLoggedInUserCart);
 
 // Checkout current user's cart
-cartsRouter.post('/checkout', util.checkAuth, db.checkoutCurrentCart);
+cartsRouter.post("/checkout", util.checkAuth, db.checkoutCurrentCart);
 
 module.exports = cartsRouter;
